b4d24203651699471efaca865641d570bf9596eb
sameerhamid <93476000+sameerhamid@users.noreply.github.com>
2023-09-23 14:57:06 +0000

    functions basics done

---
+++ jsPrograms/functions.js
@@ -0,0 +1,45 @@
+// // function addTwoNumbers (num1,num2){
+// //     console.log(num1+num2);
+// // }
+
+// function addTwoNumbers (num1,num2){
+//     return num1+num2
+// }
+
+// const res = addTwoNumbers(1,2)
+
+// console.log(res);
+
+// function loginUserMessage(userName){
+//     return `${userName} just loged in`
+// }
+
+// // console.log(loginUserMessage('sameer'))
+// console.log(loginUserMessage())
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(100,200,300))
+
+// const user = {
+//     userName:'sam',
+//     price:200,
+
+// }
+
+// function handleObject(anyObject){
+//     console.log(`UserName is ${anyObject.userName} and price is ${anyObject.price}`)
+// }
+
+// handleObject(user)
+
+const arr = [1,2,3,4]
+
+function handleArr (myArr){
+    return myArr[2]
+}
+
+// console.log(handleArr(arr));
+console.log(handleArr([1,2,3,4]));
