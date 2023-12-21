88c93fc716a2540b7efcc7ad520e239b64cf1553
your_github_username <your_github_email>
2023-12-21 10:04:27 +0530

    some more methods in arrays

---
+++ jsPrograms/Array_2.js
@@ -0,0 +1,26 @@
+const marvel_heros = ['Thor', "Ironman", 'Spiderman']
+const dc_heros = ["Superman", 'Falsh', "Batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+
+// const newArr = marvel_heros.concat(dc_heros)
+
+// console.log(newArr);
+
+// const newArr = [...marvel_heros, ...dc_heros]
+
+// console.log(newArr);
+
+// const another_arr = [1, 2, 5, [4, 6, 7], 8, [3, [5, 6, 9, 0]]]
+
+// const flatenArr = another_arr.flat(Infinity)
+
+// console.log(flatenArr);
+
+console.log(Array.isArray("sameer"));
+console.log(Array.from("sameer"));
+console.log(Array.from({ name: "sameer" }));
+
+console.log(Array.of(1, 3, 4, 5));
