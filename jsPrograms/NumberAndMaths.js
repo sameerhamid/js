d2b4a3e0374cbc21555f7fd513c8c3bd72c8138b
your_github_username <your_github_email>
2023-12-21 08:57:51 +0530

    math and num

---
+++ jsPrograms/NumberAndMaths.js
@@ -0,0 +1,33 @@
+// const score = 200
+
+// console.log(score);
+
+// const balance = new Number(3000)
+
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// const hundrands = 10000000
+
+// console.log(hundrands.toLocaleString('en-IN'));
+
+
+
+// --------------------maths--------------------
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(34.55));
+// console.log(Math.floor(34.55));
+// console.log(Math.ceil(34.55));
+
+console.log(Math.random());
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10;
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
